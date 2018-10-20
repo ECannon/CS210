41fb1afc37e0b1336ee4e98e1a76b85966180066
Eric Cannon <ericcannon@outlook.com>
2018-10-20 15:22:14 +0100

    Luhn Algorithm

---
+++ Lab1.java
@@ -0,0 +1,40 @@
+//Luhn's Algorithm
+
+import java.util.Scanner;
+
+public class Lab1{
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.nextLine();
+		System.out.println(luhnsAlgorithm(s1));
+	}
+	
+	public static boolean luhnsAlgorithm(String s1){
+		int sum = 0;
+		boolean flag = true;
+		
+		for(int i = 0; i < s1.length(); i ++){
+			int d = Integer.parseInt(s1.substring(i, i + 1)) * 2;			
+
+			if(flag)
+				sum += Integer.parseInt(s1.substring(i, i+1));
+			else{				
+				if(d > 9){
+					int num1 = d % 10;
+					int num2 = d/10 %10;
+					sum += num1 + num2;
+				}
+				else
+					sum += d;
+			}
+			
+			flag = !flag;		
+		}
+			
+		if(sum % 10 == 0)
+			return true;
+		else
+			return false;
+	}
+}
